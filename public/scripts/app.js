872a3998e928e8e51da75fb761d362ddbc4668ee
dave-benson <39385427+dave-benson@users.noreply.github.com>
2018-05-19 21:21:43 +0100

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,7 +7,7 @@
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
-var deadline = new Date("dec 31, 2018 15:37:25").getTime();
+var deadline = new Date("jul 18, 2020 16:00:00").getTime();
  
 var x = setInterval(function() {
  
