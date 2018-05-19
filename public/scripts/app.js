b21e848079193b7961c0a6bd3e9d6bd56d5ffa86
dave-benson <39385427+dave-benson@users.noreply.github.com>
2018-05-19 20:29:30 +0100

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -65,7 +65,7 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date("Sep 5, 2018 15:37:25").getTime();
 initializeClock('clockdiv', deadline);
       
         display.textContent = minutes + ":" + seconds; 
