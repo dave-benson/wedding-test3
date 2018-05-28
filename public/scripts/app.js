087e75cb773773d8ed01df78743fab1a7c4a570d
dave-benson <39385427+dave-benson@users.noreply.github.com>
2018-05-28 19:00:00 +0100

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,36 +1,10 @@
-logoSize = function () {
-    // Get the real width of the logo image
-    var theLogo = $("#thelogo");
-    var newImage = new Image();
-    newImage.src = theLogo.attr("src");
-    var imgWidth = newImage.width;
-    
-    // distance over which zoom effect takes place
-    var maxScrollDistance = 700;
-    
-    // set to window height if larger
-    maxScrollDistance = Math.min(maxScrollDistance, $(window).height());
-    
-    // width at maximum zoom out (i.e. when window has scrolled maxScrollDistance)
-    var widthAtMax = 100;
-    
-    // calculate diff and how many pixels to zoom per pixel scrolled
-    var widthDiff = imgWidth - widthAtMax;
-    var pixelsPerScroll =(widthDiff / maxScrollDistance);
-
-    $(window).scroll(function () {
-        // the currently scrolled-to position - max-out at maxScrollDistance
-        var scrollTopPos = Math.min($(document).scrollTop(), maxScrollDistance);
-        
-        // how many pixels to adjust by
-        var scrollChangePx =  Math.floor(scrollTopPos * pixelsPerScroll);
-        
-        // calculate the new width
-        var zoomedWidth = imgWidth - scrollChangePx;
-        
-        // set the width
-        $('.logo').css('width', zoomedWidth);
-    });
-}
-
-logoSize();
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("body").addClass("sticky-header");
+    } else {
+      $("body").removeClass("sticky-header");
+    }
+  })
+})
