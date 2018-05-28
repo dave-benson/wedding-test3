1b783145936e7b9f0d56605e502157be8051e154
dave-benson <39385427+dave-benson@users.noreply.github.com>
2018-05-28 18:09:41 +0100

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -46,14 +46,22 @@ if (t < 0) {
 
 
 
-/* $(function() {
-  $(window).scroll(function() {
-    var winTop = $(window).scrollTop();
-    if (winTop >= 30) {
-      $("body").addClass("sticky-header");
-    } else {
-      $("body").removeClass("sticky-header");
-    }
-    }); */
 
 
+
+
+
+function parallax_height() {
+  var scroll_top = $(this).scrollTop();
+  var sample_section_top = $(".parallax-section").offset().top;
+  var header_height = $(".parallax-header-section").outerHeight();
+  $(".sample-section").css({ "margin-top": header_height });
+  $(".sample-header").css({ height: header_height - scroll_top });
+}
+parallax_height();
+$(window).scroll(function() {
+  parallax_height();
+});
+$(window).resize(function() {
+  parallax_height();
+});
